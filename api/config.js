5a59d1eb6def8a519410d4162c70470bbb3dfea1
Mathilde <maguillemain@unistra.fr>
2025-10-30 16:06:58 +0100

    Modification fichiers

--- api/config.js
+++ api/config.js
@@ -1,11 +1,11 @@
 module.exports =  {
     ACCESS_TOKEN_SECRET : "EMMA123",
     BDD : {
-    "host" :"dpg-d40798bipnbc73cf4njg-a.oregon-postgres.render.com",
+    "host" :"dpg-d41mvfndiees73ejoghg-a.oregon-postgres.render.com",
     "port" : "5432",
-    "user" : "cnam_qnjc_user",
-    "password" : "FOYjmLxOmqwdnfGPhMO4R8eZpErPzVXQ",
-    "bdname" :"cnam_qnjc" 
+    "user" : "pollutions_w3ev_user",
+    "password" : "sabhUWK6nSC4f0v72ZwKSb49OCc4QQW1",
+    "bdname" :"pollutions_w3ev" 
     }
 }
 
