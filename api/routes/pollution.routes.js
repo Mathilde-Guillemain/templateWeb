6f066770b37b1e551cab750b04917813d36d1564
Mathilde <maguillemain@unistra.fr>
2026-01-09 11:58:38 +0100

    Modif pour tp7

--- api/routes/pollution.routes.js
+++ api/routes/pollution.routes.js
@@ -1,13 +1,17 @@
 module.exports = app => {
     const pollution = require("../controllers/pollution.controllers.js");
+    const authMiddleware = require("../middleware/auth.middleware.js");
   
     var router = require("express").Router();
   
+  // Routes publiques
   router.get("/", pollution.get);
   router.get("/:id", pollution.getOne);
-  router.post("/", pollution.create);
-  router.put("/:id", pollution.update);
-  router.delete("/:id", pollution.delete);
+  
+  // Routes protégées - nécessitent authentification
+  router.post("/", authMiddleware, pollution.create);
+  router.put("/:id", authMiddleware, pollution.update);
+  router.delete("/:id", authMiddleware, pollution.delete);
 
   app.use('/api/pollution', router);
   };
